4306ed81450eac982bb92970ae108bed578d4792
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2021-01-20 19:15:20 -0600

    completed spinner/magic eight ball android app

---
+++ app/src/main/java/com/example/relearnjava/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.relearnjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView spinner;
+    private Random random = new Random();
+    private int lastDir;
+    private boolean spinning;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+    }
+    public void spinCircle(View v) {
+        if(!spinning) {
+            int newDir = random.nextInt(1800);
+            float pivotX = spinner.getWidth() / 2;
+            float pivotY = spinner.getHeight() / 2;
+
+            Animation rotate = new RotateAnimation(lastDir, newDir, pivotX, pivotY);
+            rotate.setDuration(2500);
+            rotate.setFillAfter(true);
+            rotate.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spinning = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            lastDir = newDir;
+            spinner.startAnimation(rotate);
+        }
+    }
+}
